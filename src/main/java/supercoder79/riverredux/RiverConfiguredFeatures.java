ce55ef4635e75405a65908e44bbfed9df849dcf3
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2021-06-03 00:40:15 -0400

    Port to 1.17-pre4

--- src/main/java/supercoder79/riverredux/RiverConfiguredFeatures.java
+++ src/main/java/supercoder79/riverredux/RiverConfiguredFeatures.java
@@ -12,20 +12,21 @@ import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.ConfiguredFeatures;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.SpringFeatureConfig;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 
 public class RiverConfiguredFeatures {
     public static final ConfiguredFeature<?, ?> RIVER_WATERFALLS = Feature.SPRING_FEATURE.configure(
             new SpringFeatureConfig(Blocks.WATER.getDefaultState().getFluidState(), true, 4, 1,
                     ImmutableSet.of(Blocks.STONE, Blocks.GRANITE, Blocks.DIORITE, Blocks.ANDESITE, Blocks.GRAVEL, Blocks.DIRT)))
-            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(YOffset.fixed(64), YOffset.fixed(128))))
+            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.fixed(64), YOffset.fixed(128)))))
             .spreadHorizontally().repeat(128).repeat(8);
 
     public static final ConfiguredFeature<?, ?> RIVER_SUGARCANE = Feature.RANDOM_PATCH.configure(ConfiguredFeatures.Configs.SUGAR_CANE_CONFIG)
-            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(YOffset.fixed(60), YOffset.fixed(64))))
+            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.fixed(60), YOffset.fixed(64)))))
             .spreadHorizontally().repeat(16);
 
     public static final ConfiguredFeature<?, ?> RIVER_SUGARCANE_TROPICAL = Feature.RANDOM_PATCH.configure(ConfiguredFeatures.Configs.SUGAR_CANE_CONFIG)
-            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(YOffset.fixed(60), YOffset.fixed(64))))
+            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.fixed(60), YOffset.fixed(64)))))
             .spreadHorizontally().repeat(48);
 
     public static void init() {
